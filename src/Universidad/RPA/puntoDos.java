5465f8cb2745b43262a16511673f1909977c15fd
SachaCoggiola <scbalgane@gmail.com>
2025-04-14 23:09:15 -0300

    ejercicio 2 tp2

--- src/Universidad/RPA/puntoDos.java
+++ src/Universidad/RPA/puntoDos.java
@@ -1,7 +1,15 @@
 package Universidad.RPA;
 
+import java.util.Scanner;
+
 public class puntoDos {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una temperatura en fahrenheit (solo ingrese el numero)");
+        float fahrenheit = sc.nextFloat();
+        System.out.println("La temperatura expresada en grados Celcius: " + celciusToFahrenheit(fahrenheit));
+    }
+    public static float celciusToFahrenheit(float celcius) {
+        return celcius * 9/5 + 32;
     }
 }
