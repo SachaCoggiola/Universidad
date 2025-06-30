4d9aa3f3dc4ded10c3740d800162324a89b73d62
SachaCoggiola <scbalgane@gmail.com>
2025-06-30 00:28:05 -0300

    Trabajo Practico Final

--- src/Universidad/RPA/tpFinal.java
+++ src/Universidad/RPA/tpFinal.java
@@ -7,8 +7,7 @@ public class tpFinal {
         Scanner sc = new Scanner(System.in);
         String jugadorUno, jugadorDos;
         double puntajeUno = 0, puntajeDos = 0, maxPuntUno = 0, maxPuntDos = 0;
-        int cantRonda = 0, valorCarta, rondaJugUno = 1, rondaJugDos = 1;
-        boolean turno = true;
+        int cantRonda = 0, valorCarta , rondaJugUno = 1, rondaJugDos = 1;
         System.out.println("Ingrese el nombre del jugador N°1:");
         jugadorUno = sc.nextLine();
         System.out.println("Ingrese el nombre del jugador N°2:");
@@ -16,28 +15,10 @@ public class tpFinal {
         do {
             cantRonda++;
             System.out.println("Ha comenzado la ronda N°"+cantRonda);
-            for (int i = 0; i < 2; i++) {//modularizar el turno? no cambia nada solo queda mas lindo?
-                if(turno){
-                    System.out.println("Es turno del jugador "+jugadorUno);
-                }else{
-                    System.out.println("Es turno del jugador "+jugadorDos);
-                }
-                double puntaje = 0;
-                do {
-                    System.out.println("Ingrese el valor de la carta:");
-                    valorCarta = sc.nextInt();
-                    valorCarta = verificarCarta(valorCarta);
-                    puntaje += calcularPuntaje(valorCarta);
-                    System.out.println(puntaje);
-                } while (valorCarta != 1 && valorCarta != 12);
-                if (turno) {
-                    puntajeUno += puntaje;
-                    turno = false;
-                } else {
-                    puntajeDos += puntaje;
-                    turno = true;
-                }
-            }
+            System.out.println("Es turno del jugador "+jugadorUno);
+            puntajeUno += turno(0);
+            System.out.println("Es turno del jugador "+jugadorDos);
+            puntajeDos += turno(0);
             if (maxPuntUno < puntajeUno) { //modularizar? para que no quede feo
                 maxPuntUno = puntajeUno;
                 rondaJugUno = cantRonda;
@@ -46,7 +27,7 @@ public class tpFinal {
                 maxPuntDos = puntajeDos;
                 rondaJugDos = cantRonda;
             }
-        }while(puntajeUno <= 60 && puntajeDos <= 60);
+        }while(puntajeUno < 60 && puntajeDos < 60);
         if(puntajeUno == puntajeDos){
             System.out.println("Los jugadores "+ jugadorUno +" y "+ jugadorDos +" han empatado con un total de "+puntajeUno);
             System.out.println("Han jugado un total de "+cantRonda+" rondas.");
@@ -80,5 +61,16 @@ public class tpFinal {
     }
     public static boolean esPar(int valorCarta){
         return valorCarta % 2 == 0;
+    }
+    public static double turno(int valorCarta){
+        double puntaje = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese el valor de la carta:");
+            valorCarta = sc.nextInt();
+            valorCarta = verificarCarta(valorCarta);
+            puntaje += calcularPuntaje(valorCarta);
+        } while (valorCarta != 1 && valorCarta != 12);
+        return puntaje;
     }
 }
